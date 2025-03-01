c4c4905acffcc9af0634338bfb54479145ad016f
FabianVladmir <fflorez_16@hotmail.com>
2025-03-01 05:52:43 -0500

    Integrando handsontable

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -7,6 +7,12 @@ const ExpensesProfitsPage = React.lazy(
   () => import('../views/expenses-profits/pages/ExpensesProfits'),
 )
 
+// Tabla ejemplo
+const ExampleComponent  = React.lazy(
+  () => import('../views/expenses-profits/pages/TreeTable'),
+)
+
+
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   /* { path: '/dashboard', name: 'Dashboard', element: Dashboard }, */
@@ -14,6 +20,11 @@ const routes = [
     path: '/gastos-ganancias',
     name: 'Gastos y ganancias',
     element: ExpensesProfitsPage,
+  },
+  {
+    path: '/tabla-anidada',
+    name: 'Tabla',
+    element: ExampleComponent,
   },
 ]
 
