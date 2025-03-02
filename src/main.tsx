fe3ad7d017f3e7be355037b251de66e1ddd12846
FabianVladmir <fflorez_16@hotmail.com>
2025-03-02 13:42:16 -0500

    Integrando handsontable

--- src/main.tsx
+++ src/main.tsx
@@ -11,12 +11,18 @@ const rootElement = document.getElementById('root')
 
 if (rootElement) {
   createRoot(rootElement).render(
+<<<<<<< HEAD
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <Provider store={store}>
         <App />
       </Provider>
     </QueryClientProvider>,
+=======
+    <Provider store={store}>
+      <App />
+    </Provider>
+>>>>>>> e5a4e8f (Integrando handsontable)
   )
 } else {
   console.error("No se encontró el elemento con id 'root'.")
